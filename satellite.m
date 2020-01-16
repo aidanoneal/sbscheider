587d235b06982dd39b985191529e8acb50bd46cd
SBscheider3 <sbscheider@yahoo.com>
2020-01-16 00:11:30 -0500

    Update satellite.m

--- satellite.m
+++ satellite.m
@@ -9,10 +9,10 @@ M = 5.98*10^(24);
 
 T =T*60; % convert to sec 
 h = (G*M*T^2/(4*pi^2))^(1/3)-R; %height in meters
-h = round(h/1000,0) %convert answer to Kilometers and round to nerest integer
+h = round(h/1000,0) %convert answer to Kilometers and round to nearest integer
 
 
 V = sqrt((G * M)/(R + (h * 1000))); %calculate velocity at respective h
 
-V = round(V,0) %round to nerest integer
+V = round(V,0) %round to nearest integer
 
